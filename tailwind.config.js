ca1039693363ac6d470e1c36e75024ab4a69ffaa
IndraW02 <iwirawan54@gmail.com>
2025-01-03 10:17:11 +0700

    Update index, styles, and Tailwind config for new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)',
+          },
+          '50%': {
+            transform: 'rotate(10deg)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
